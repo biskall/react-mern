b0eaffb0a4c5c33c5249afdfa61fe12ffdec51d3
biskall <itaibiskall@gmail.com>
2022-12-11 13:38:04 +0200

    two users

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -8,6 +8,12 @@ const Users = () => {
     name: "John Smith",
     image: 'https://media.istockphoto.com/photos/shot-of-a-handsome-young-man-using-his-smartphone-to-send-a-text-picture-id1358205704?b=1&k=20&m=1358205704&s=612x612&w=0&h=kHjEWrYIvZyl8QxxrxrWC2L4Lkq2lcl3Rp9_V_2dIRM=',
     places: 3
+  },
+  {
+    id: "u1",
+    name: "John Smith",
+    image: 'https://media.istockphoto.com/photos/shot-of-a-handsome-young-man-using-his-smartphone-to-send-a-text-picture-id1358205704?b=1&k=20&m=1358205704&s=612x612&w=0&h=kHjEWrYIvZyl8QxxrxrWC2L4Lkq2lcl3Rp9_V_2dIRM=',
+    places: 3
   }
 ]
 
