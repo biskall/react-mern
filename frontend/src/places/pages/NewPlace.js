156b567f218b084835e59499dfd68285749bbbba
biskall <itaibiskall@gmail.com>
2022-12-21 12:59:22 +0200

    Adding Multiple Inputs

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useCallback } from "react";
 
-import Input from '../../shared/components/FormElements/Input';
-import "./NewPlace.css"
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/components/UIElement/util/validators";
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <form className="place-form">
-    <Input element='input' type="text" label="Title" validators={[]} errorText="Please enter a valid title"/>
-  </form>
-}
+   const titleInputHandler = useCallback(( id, value, isValid) => {
+
+   },[])
+
+   const descriptionInputHandler = useCallback(( id, value, isValid) => {
+
+   },[])
+
+  return (
+    <form className="place-form">
+      <Input
+        id= "input"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id= "description"
+        element="description"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH()]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
+      />
+    </form>
+  );
+};
 
 export default NewPlace;
--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -1,5 +1,6 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
+import { validate } from "../UIElement/util/validators";
 import "./Input.css";
 
 const inputReducer = (state, action) => {
@@ -8,8 +9,13 @@ const inputReducer = (state, action) => {
       return {
         ...state,
         value: action.val,
-        isValid: true,
+        isValid: validate(action.val, action.validators),
       };
+    case "TOUCH":
+        return{
+            ...state,
+            isTouched: true
+        }
     default:
       return state;
   }
@@ -19,10 +25,28 @@ const Input = (props) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: "",
     isValid: false,
+    isTouched: false,
   });
 
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+   onInput(id, value, isValid)
+  },[id, value, isValid, onInput])
+
   const changeHandler = (event) => {
-    dispatch({ type: "CHANGE", val: event.target.value });
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
+
+  const touchHandler = (event) => {
+    dispatch({
+      type: "TOUCH"
+    });
   };
 
   const element =
@@ -31,6 +55,7 @@ const Input = (props) => {
         id={props.id}
         type={props.type}
         placeholder={props.placeholder}
+        onBlur={touchHandler}
         onChange={changeHandler}
         value={inputState.value}
       />
@@ -43,10 +68,13 @@ const Input = (props) => {
       />
     );
   return (
-    <div className={`form-control ${!inputState.isValid && `form-control--invalid`}`}>
+    <div
+      className={`form-control ${!inputState.isValid && inputState.isTouched &&
+        `form-control--invalid`}`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
-      {!inputState.isValid && <p>{props.errorText}</p>}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
     </div>
   );
 };
